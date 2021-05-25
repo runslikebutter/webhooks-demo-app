054fd1a4ce3dea728b6dd2d7bc46310e2a674a34
Yurii Sekretar <yuri.sekretar@butterflymx.com>
2021-05-24 22:53:20 -0700

    Added APNs lib

--- webhooks/views.py
+++ webhooks/views.py
@@ -2,7 +2,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from pyfcm import FCMNotification
-
+from apns2.client import APNsClient
+from apns2.payload import Payload
 
 fcm_token = None
 apns_token = None
@@ -20,6 +21,14 @@ class WebhookHandlerView(APIView):
             message_title = "Test title"
             message_body = "Test body"
             result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
+            print(result)
+        if apns_token:
+            token_hex = apns_token
+            payload = Payload(alert="Test alert", sound="default", badge=1)
+            topic = 'com.demo.app'
+            client = APNsClient('key.pem', use_sandbox=True, use_alternative_port=False)
+            client.send_notification(token_hex, payload, topic)
+
         return Response(status=status.HTTP_200_OK)
 
 
