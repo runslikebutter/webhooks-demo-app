baaade65c559718d9dafcb1d9cd52ed2c2b01b29
Yurii Sekretar <yuri.sekretar@butterflymx.com>
2021-05-25 22:18:51 -0700

    Fixed guid parsing

--- webhooks/views.py
+++ webhooks/views.py
@@ -4,6 +4,8 @@ from rest_framework import status
 from pyfcm import FCMNotification
 from apns2.client import APNsClient
 from apns2.payload import Payload
+import os
+import traceback
 
 fcm_token = None
 apns_token = None
@@ -15,19 +17,29 @@ class WebhookHandlerView(APIView):
         global fcm_token
         global voip_token
         global apns_token
+        print("fcm_token - ", fcm_token)
+        print("apns_token - ", apns_token)
+        print("voip_token - ", voip_token)
         if fcm_token:
-            push_service = FCMNotification(api_key="<api-key>")
-            registration_id = fcm_token
-            message_title = "Test title"
-            message_body = "Test body"
-            result = push_service.notify_single_device(registration_id=registration_id, message_title=message_title, message_body=message_body)
-            print(result)
-        if apns_token:
-            token_hex = apns_token
-            payload = Payload(alert="Test alert", sound="default", badge=1)
-            topic = 'com.demo.app'
-            client = APNsClient('key.pem', use_sandbox=True, use_alternative_port=False)
-            client.send_notification(token_hex, payload, topic)
+            try:
+                guid = request.data["guid"]
+                push_service = FCMNotification(api_key=os.getenv('FCM_KEY'))
+                registration_id = fcm_token
+                result = push_service.notify_single_device(registration_id=registration_id, data_message={"guid": guid})
+                print(result)
+                print("guid - ", guid)
+            except Exception as ex:
+                print("Exception - ", traceback.format_exc())
+
+        if voip_token:
+            try:
+                guid = request.data["guid"]
+                payload = Payload(badge=1, custom={"guid": guid})
+                client = APNsClient(os.getenv('APPLE_VOIP_CERT_PATH'), use_sandbox=bool(os.getenv('APPLE_SANDBOX')), use_alternative_port=False)
+                client.send_notification(voip_token, payload)
+                print("guid - ", guid)
+            except Exception as ex:
+                print("Exception - ", traceback.format_exc())
 
         return Response(status=status.HTTP_200_OK)
 
@@ -43,12 +55,16 @@ class RegisterDeviceView(APIView):
         #     "type": "voip",
         #     "token": "1234"
         # }
+        # {
+        #     "device": "ios",
+        #     "type": "voip",
+        #     "token": "1234"
+        # }
         if request.data["device"] == "android":
             fcm_token = request.data.get("token", "")
         if request.data["device"] == "ios":
-            if request.data["type"] == "voip":
+            if request.data.get("type",'') == "voip":
                 voip_token = request.data.get("token", "")
             else:
                 apns_token = request.data.get("token", "")
-            fcm_token = request.data.get("token", "")
         return Response(status=status.HTTP_200_OK)
