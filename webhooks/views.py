2ccaf37bb3320d15f7bba08958119238f9f356ee
Yurii Sekretar <yuri.sekretar@butterflymx.com>
2022-01-31 07:36:04 -0800

    Feat: apns and call status support(#1)
    * Update for APNs
    
    * Implement Android demo app
    
    * Update views.py
    
    * Update views.py
    
    * Update views.py

--- webhooks/views.py
+++ webhooks/views.py
@@ -16,8 +16,12 @@ class WebhookHandlerView(APIView):
     def post(self, request):
         print(request.data)
         guid = request.data.get("guid", None)
+        push_type = "call"
+        call_status = "initializing"
         if guid is None:
+            push_type = "status"
             call = request.data.get("call", None)
+            call_status = request.data.get("status", None)
             guid = call.get("guid", None)
         token = request.GET.get('token')
         # type values: voip/apns/fcm
@@ -25,10 +29,16 @@ class WebhookHandlerView(APIView):
         print("token - ", token)
         print("type - ", type)
         print("guid - ", guid)
+        print("call_status - ", call_status)
         if type == "fcm":
             try:
                 push_service = FCMNotification(api_key=os.getenv('FCM_KEY'))
-                result = push_service.notify_single_device(registration_id=token, data_message={"guid": guid})
+                if push_type == "call":
+                    result = push_service.notify_single_device(registration_id=token,
+                                                               data_message={"guid": guid, "call_status": call_status})
+                else:
+                    result = push_service.notify_single_device(registration_id=token,
+                                                               data_message={"guid": guid, "call_status": call_status})
                 print(result)
             except Exception as ex:
                 print("Exception - ", traceback.format_exc())
@@ -40,6 +50,18 @@ class WebhookHandlerView(APIView):
                 client.send_notification(token, payload)
             except Exception as ex:
                 print("Exception - ", traceback.format_exc())
+
+        if type == "apns":
+            try:
+                if push_type == "call":
+                    payload = Payload(badge=1, alert="New call", custom={"guid": guid})
+                else:
+                    payload = Payload(badge=1, alert="Call status: " + call_status, custom={"guid": guid})
+                client = APNsClient(os.getenv('APPLE_APNS_CERT_PATH'), use_sandbox=bool(os.getenv('APPLE_SANDBOX')), use_alternative_port=False)
+                client.send_notification(token, payload, topic="com.butterflymx.sdk.demo")
+            except Exception as ex:
+                print("Exception - ", traceback.format_exc())
+
 
         return Response(status=status.HTTP_200_OK)
 
